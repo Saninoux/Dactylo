7d78d05d4c19b86d99b45a2a75f3d8c76195b3cb
Aarush <aarush.jawdekar@gmail.com>
2019-03-13 11:26:51 +0100

    Add files via upload

---
+++ theory_dactylo.py
@@ -0,0 +1,82 @@
+"""
+Module theorie
+"""
+
+un = """
+     jfjf fjfjfjfffj ffjfjfjf fjfjfjjjjjfjfffjfjfjff fjfffjfjfjfjfffjf fjfjfjfff fjfjfjfjfjfjj
+     jfjfjfjfjfjfjfjfj jfjffffjjjjjj jfjfjfjfjfjfj fjfjfjjfjf jjfjfjfff ffjfjfjffjf jfjfjjffff
+     jfj jfjfjjfjfj jfjfjfjfjfjf fffjfjf fjfjfjjfjjfjfjf fjfjjfjfjjfjjfjfjfjfjfjfjf jjjjjjjjff
+     jfjfjf jfjjf
+     """
+
+deux = """
+       fdfddd jfkddf kdjfkdjfddkjk ddfdfdkjfkdjkfj dddjjjjkkdd fjkdkfjfdkdfjfjfkkk jdkfjddd kj
+       kjdkfjkdf dkkkkkfdfkjdjkjf kjdkfjdkjkfkdfkd kjdfkjfkdjf dfffffkjdkkkkkk djfkjdkkkfkdf k
+       kjdkjfdkdfk fkdkjfdkjfkkjkjkjkjkkjd fkdjfkdjfkdjkfkdjkfjdkf kdjkfjdkfdjkfjdkjfkjdkfkkdf
+       kjdkddfkkjkd
+       """
+
+trois = """
+        sfkdljsfdls fdsdsdkljlkjdsfkljsklfjsd kljsdkfsdfsdjk lkjsdlkjfsdkljsdklf lskdjflksdjlj
+        lkjlksddfslkdjs kljsdf lkfsdjlksdklklkdsjflksdjdf lkjsldkjfsdklsfd lkjklsdfddsfdslkjll
+        klsljlfsklsdjfksd ldskjfdlskjflskjldfs lksdsdfkljsdfkljds flkdsjlkfdjfsdfljksfd fsdlkj
+        kljdsfkfldss
+        """
+        
+quatre = """
+         aéslkdfjfaskjldas fslkfsdsdfklsdf asfdkljsadfsdsdfaasfd kljklsfdfsd sdffsdlkjéjslfjél
+         léjkfsdaskfldfsdalkjsdlkjasjklsadf lkjsjass élaéaskfljs kljsadfélasdkjskadj lkjlkjsaf
+         lékjasffs sfsadélksjafdlkjlkjsf skléafjfséalkjf lskfjdédskjlakljakljaskl jéasfasdélkj
+         jkélasdfa fs
+         """
+
+cinq = """
+       aslékjfdtzatlké jzklaéfzé alkjtklzaflkjéafdatéjkl zéafzatjatéjlj falkztzttazlkatszdk jk
+       alfésjkttzaltkfjaélskfdjds atztztalkjffjddasflj attkzlfkaldfjfadéslkjaéfkj affsdsadfsdf
+       afkljakféfsldjélafkljédfatztztz aélfkjfsjdl ztakljféfsékltztztztj kélasfkljasfaé tzkaéa
+       asdfjlkatélk
+       """
+
+six = """
+      asékfljbnénj ékljasflknbnjnasbfjé naéjsbbslajbfsnlfbasnjln slfjsdjlnfasbajénljalsknb bbb
+      aéjanfdkdjénklfsjabjfanéjnjfasjdlka sjnbéasjfbsjéasnfljsfa jlnsaflbjsajn
+      aéjanfdkdjénklfsjabjfanéjnjfasjdlka sjnbéasjfbsjéasnfljsfa jlnsaflbjsajn
+      """
+
+sept = """
+
+       """
+
+huit = """
+
+       """
+
+neuf = """
+
+       """
+
+dix = """
+
+      """
+
+onze = """
+
+       """
+
+douze = """
+
+        """
+
+treize = """
+
+         """
+
+quatorze = """
+            
+           """
+
+quinze = """
+
+         """
+
+print(one)
