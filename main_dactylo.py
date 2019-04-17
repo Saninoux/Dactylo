b7fc25c30387878b9dae6761f688d1777740bd79
Aarush <aarush.jawdekar@gmail.com>
2019-04-17 11:11:54 +0200

    Add files via upload
    squelette du programme principal

---
+++ main_dactylo.py
@@ -0,0 +1,34 @@
+"""
+Programme dactylo
+"""
+import curses
+
+
+def menu(console):
+    """Fonction qui affiche menu principal."""
+    
+    
+def timer():
+    """Fonction qui gère le timer."""
+    
+    
+def apprendre():
+    """Fonction qui gère la partie "apprendre"."""
+    
+   
+def jouer():
+    """Fonction qui gère la partie "jouer"."""
+    
+
+print("Voulez vous:")
+print("1) Apprendre")
+print("2) Jouer")
+choix = input(">>> ")
+
+if choix == 1:
+    apprendre()
+    break
+
+elif choix == 2:
+    jouer()
+    break 
