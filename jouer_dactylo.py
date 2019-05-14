95598c5e50c5fceab876aa6070be2768ab0ae560
Aarush <aarush.jawdekar@gmail.com>
2019-05-14 18:26:26 +0200

    Update jouer_dactylo.py

--- jouer_dactylo.py
+++ jouer_dactylo.py
@@ -7,7 +7,10 @@ Partie jouer
 import random
 import time 
 
-TEXTE = ("""Recopiez le mot et appuyez sur Enter.""")
+# On définit les constantes
+TEXTE = ("""Recopiez le mot qui vous sera donné et appuyez sur Enter. 
+Le but du jeu est d'écrire 30 mots dans le moins de temps possible.
+Plusieurs niveaux de difficulté vous sont proposés.""")
 LISTE_MOTS_1 = ["sera", "forger", "dictionnaire", "entre", "moule", "mecs", "douanier", 
                 "effets", "bercez", "institut", "oasis", "persifleur", "sel", "tiens",
                 "ramifications", "porter", "lots", "embarquer", "insultant", "publiait",
@@ -24,71 +27,79 @@ LISTE_MOTS_2 = ["Tony", "Robert", "Steve", "Chris", "Natasha", "Scarlett", "Bruc
                 "Lisa", "Homer", "Bart", "Marge", "Maggie", "Abe", "Apu", "Milhouse"]
 LISTE_MOTS_3 = ["Épée", "Armée", "Trône", "Été", "Traître", "détrôner"]
 
+print(TEXTE)
+
 
 def jouer_1():
     """Fonction qui gère le niveau 1."""
+    # On initialise le score
     score = 0
-    print(TEXTE)
     while True:
+        # On initialise le compteur
         start = time.time()
         mot = random.choice(LISTE_MOTS_1)
         print("Mot: {}".format(mot))
         reponse = input(">>> ")
         if reponse == mot:
+            # On ajoute 1 au score si la réponse est juste
             score += 1
-            print("Score: {}".format(score))
+            # tout en indiquant le score actuel
+            print("Mots écrits: {}".format(score))
+            if score == 30:
+                # On arrête le compteur
+                end = time.time()
+                score_final = end - start
+                # On affiche le score final
+                print("Vous avez pris {} secondes.".format(score_final))
+                break
+        else:
+            # On indique à l'utilisateur qu'il n'a pas eu le point
+            print("Faux!")
+
+
+def jouer_2():
+    """Fonction qui gère le niveau 1."""
+    score = 0
+    while True:
+        start = time.time()
+        mot = random.choice(LISTE_MOTS_2) # Ici, on prend la liste de mots 
+        print("Mot: {}".format(mot))      # avec une difficulté moyenne
+        reponse = input(">>> ")
+        if reponse == mot:
+            score += 1
+            print("Mots écrits: {}".format(score))
             if score == 30:
                 end = time.time()
-                score_final = round(end - start)
-                print("Vous avez pris {} secondes pour écrire 30 mots.".format(score_final))
+                score_final = end - start
+                print("Vous avez pris {} secondes.".format(score_final))
+                break
+        else:
+            print("Faux!")
+            
+            
+def jouer_3():
+    """Fonction qui gère le niveau 1."""
+    score = 0
+    while True:
+        start = time.time()
+        mot = random.choice(LISTE_MOTS_3) # Ici, on prend la liste de mots 
+        print("Mot: {}".format(mot))      # avec la plus grande difficulté
+        reponse = input(">>> ")
+        if reponse == mot:
+            score += 1
+            print("Mots écrits: {}".format(score))
+            if score == 30:
+                end = time.time()
+                score_final = end - start
+                print("Vous avez pris {} secondes.".format(score_final))
                 break
         else:
             print("Faux!")
 
 
-def jouer_2():
-    """Fonction qui gère le niveau 2."""
-    score = 0
-    print(TEXTE)
-    while True:
-        start = time.time()
-        mot = random.choice(LISTE_MOTS_2)
-        print("Mot: {}".format(mot))
-        reponse = input(">>> ")
-        if reponse == mot:
-            score += 1
-            print("Score: {}".format(score))
-            if score == 100:
-                end = time.time()
-                score_final = round(end - start)
-                print("Vous avez pris {} secondes pour écrire 100 mots.".format(score_final))
-                break
-        else:
-            print("Réesayez!")
-            
-            
-def jouer_3():
-    """Fonction qui gère le niveau 3."""
-    score = 0
-    print(TEXTE)
-    while True:
-        start = time.time()
-        mot = random.choice(LISTE_MOTS_3)
-        print("Mot: {}".format(mot))
-        reponse = input(">>> ")
-        if reponse == mot:
-            score += 1
-            print("Score: {}".format(score))
-            if score == 100:
-                end = time.time()
-                score_final = round(end - start)
-                print("Vous avez pris {} secondes pour écrire 100 mots.".format(score_final))
-                break
-        else:
-            print("Réesayez!")
-
-
-niveau = int(input("Choisis un niveau (1, 2 ou 3): "))
+# On demande un niveau au joueur et on indique quel fonction prendre selon
+# son choix
+niveau = int(input("Choissisez un niveau (1, 2 ou 3): "))
 if niveau == 1:
     jouer_1()
 elif niveau == 2:
