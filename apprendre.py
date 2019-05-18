c00292720df1acba7bbc6f4e2123713beacfaa82
Saninoux <47175955+Saninoux@users.noreply.github.com>
2019-05-18 10:20:54 +0200

    Update and rename dac.py to apprendre.py

--- apprendre.py
+++ apprendre.py
@@ -1,9 +1,16 @@
+"""
+Partie "jouer" du jeu dactylographique
+dernière modification faite le 14.05.2019
+auteurs: JAWDEKAR Aarush et THAY San
+"""
+
 import time
 
 MOT = "anticonstitutionnellement"
 
 
 def apprendre():
+    erreur = 0
     for lettre in range(10,len(MOT)+10):
         while True:
             print(MOT[0+lettre-10:lettre+1])
@@ -16,15 +23,28 @@ def apprendre():
                 break
             # is touche fausse
             else:
+                print("Faux!")
+                erreur += 1
+                print(erreur)
                 False
+
+    return erreur
 
 
 # pour déterminer le temps que le joueur prend pour finir la partie
-start = time.time()
-apprendre()
-end = time.time()
-print(end - start + "secondes")
+def temps_jouee():
+    """ fonction permettant au jouer de savoir son temps joué et son
+    nombre d'erreurs """
+    start = time.time()
+    err = apprendre()
+    end = time.time()
+    temps_jouee = int(round(end - start))
+    print("Vous avez pris {}, avec {} erreurs.".format(temps_jouee, err))
+
+
+def main():
+    temps_jouee()
 
 
 if __name__ == '__main__':
-    apprendre()
+    main()
