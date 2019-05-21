05115dad85b5725a4a4876eb9023066c02e2f08d
Aarush <aarush.jawdekar@gmail.com>
2019-05-21 20:05:19 +0200

    Update doc_temps_jouee.py

--- doc_temps_jouee.py
+++ doc_temps_jouee.py
@@ -9,14 +9,13 @@ import doc_apprendre
 
 def temps_jouee():
     """
-    fonction permettant au jouer de savoir son temps joué et son
-    nombre d'erreurs.
+    Fonction permettant au jouer de connaître son temps joué et son
+    nombre d'erreurs
     """
     start = time.time()
     err = doc_apprendre.appeler_apprendre()
     end = time.time()
     secondes_prises = int(round(end - start))
-
     return secondes_prises, err
 
 
