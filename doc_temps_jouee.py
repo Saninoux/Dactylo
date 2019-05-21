dbdca2319851d9faa782f8abb551a8b3a5c30455
Aarush <aarush.jawdekar@gmail.com>
2019-05-21 20:04:34 +0200

    Update doc_temps_jouee.py

--- doc_temps_jouee.py
+++ doc_temps_jouee.py
@@ -1,7 +1,8 @@
 """
+Temps joué et nombre d'erreurs
+dernière modification le 21.05.2019
+auteurs: JAWDEKAR Aarush, THAY San
 """
-
-
 import time
 import doc_apprendre
 
@@ -19,8 +20,8 @@ def temps_jouee():
     return secondes_prises, err
 
 
-# pour que fonction annuler si la fonction executer
-# intentiellemnt par user
+# Pour que la fonction s'annule si elle est exécutée
+# intentionellemnt par user
 def do_something():
     pass
 
